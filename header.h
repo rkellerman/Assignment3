8a115ff3cc8e912cd4f24ccf014182e8d572f7eb
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2016-12-04 10:19:15 -0500

    Added Makefile and header file

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_GAURD
+#define HEADER_GAURD
+#define RIO_BUFSIZE 8192
+
+typedef struct {
+	int rio_fd;                /* Descriptor for this internal buf */
+	int rio_cnt;               /* Unread bytes in internal buf */
+	char *rio_bufptr;          /* Next unread byte in internal buf */
+	char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+} rio_t;
+
+#endif
